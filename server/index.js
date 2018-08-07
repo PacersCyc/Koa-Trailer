dd9b754efec73da9d427c575a43885151d789e25
PacersCyc <452720855@qq.com>
2018-08-07 21:06:12 +0800

    pug ejs try

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 
 const Koa = require('koa')
 const app = new Koa()
-const {normal} = require('./tpl')
+const {htmlTpl, ejsTpl, pugTpl} = require('./tpl')
+const ejs = require('ejs')
+const pug = require('pug')
 
 app.use(async (ctx, next)=>{
   ctx.type = 'text/html;charset=utf-8'
   //ctx.body = '电影首页'
-  ctx.body = normal
+  /*ctx.body = ejs.render(ejsTpl, {
+    you:'Luke',
+    me:'cyc'
+  })*/
+  ctx.body = pug.render(pugTpl, {
+    you:'Luke',
+    me:'cyc'
+  })
 })
 
 app.listen(4000)
